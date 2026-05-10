42dde5ffa31e044809bd5d1cdf7d23af332f30a1
zDerp01 <confused1dev@gmail.com>
2026-05-10 19:45:57 +0100

    3.2 feito

--- main.cpp
+++ main.cpp
@@ -4,20 +4,16 @@
 using namespace std;
 
 int main() {
-    // Configura o gerador de números aleatórios
     srand(time(NULL));
 
     Sector* setores = nullptr;
     Produto* armazem = nullptr;
 
-    // Inicialização do sistema
     inicializarSupermercado(setores);
     inicializarArmazem(armazem, setores);
 
-    // NOVO: Garante que as árvores de vendas começam vazias (limpa lixo de memória)
     corrigirInicializacao(setores);
 
-    // Mostra o estado inicial antes do menu aparecer
     mostrarSuper(setores, armazem);
 
     return 0;
